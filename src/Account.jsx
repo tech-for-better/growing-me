66f3f945a8ed293f20976cbf435e27b181cf9307
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-12 13:00:29 +0100

    Change process to import.meta and import React on each page
    Co-authored-by: Safia Ali <safiaali@hotmail.com

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 import "./index.css";
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import './index.css'
-import { useState, useEffect } from 'react'
-import { supabase } from './supabaseClient'
-import Auth from './Auth'
-import Account from './Account'
+import React from "react";
+import "./index.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
 
 export default function App() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    setSession(supabase.auth.session())
+    setSession(supabase.auth.session());
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
     </div>
-  )
+  );
 }
