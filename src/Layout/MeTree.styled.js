ac8eb90485f7665801053e4378ed11ffe0387a12
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-15 15:03:18 +0100

    Add components and styling to Me Tree page
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

---
+++ src/Layout/MeTree.styled.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Toolkit = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: ;
+`;
+
+export const ToolkitButton = styled.button`
+  margin-top: 1rem;
+  margin-left: 1rem;
+  color: #fff;
+  border: var(--custom-border);
+  background-color: var(--custom-bg-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-radius: var(--custom-border-radius);
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  text-align: center;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  max-width: 10rem;
+  &:hover {
+    background-color: #337d8e;
+  }
+`;
+
+export const ToolkitText = styled.p`
+  color: #fff;
+`;
+
+export const MeTreeImage = styled.img`
+  height: 75vh;
+`;
+
+export const BtnImage = styled.img`
+  height: 100px;
+  max-width: 100px;
+`;
+
+// COLOURS
+// d6f7d - darkblue
+// e8eedf - palegrey
+// daac2f - orangey
+// a5cfe4 - paleblue
+// 94b3c5 - duskyblue
+// 3d4726 - darkgreen
+// 826521- brown
+// ecf0d9- palegreen
+// acabb3 - grey
+// 67bcce - turquoiseblue
--- src/MeTree.jsx
+++ src/MeTree.jsx
@@ -2,79 +2,75 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 import { Link } from "react-router-dom";
+import {
+  Toolkit,
+  ToolkitButton,
+  MeTreeImage,
+  BtnImage,
+  ToolkitText,
+} from "./Layout/MeTree.styled";
+import MeTreeGarden from "./../assets/where_-_garden.svg";
+import MeTreeCloud from "./../assets/where_-_cloud.svg";
+import MeTreeHeart from "./../assets/where_-_on_a_big_love_heart.svg";
+import MeTreePlanet from "./../assets/where_-_another_planet.svg";
+import WhatColour from "./../assets/what_colour_is_your_tree.svg";
+import WhatGrows from "./../assets/what_grows_on_your_tree.svg";
+import WhatShape from "./../assets/what_shape_is_your_tree.svg";
+import WhereTree from "./../assets/where_is_your_tree.svg";
+import WhoAround from "./../assets/who_is_around_your_tree.svg";
 
 export default function MeTree({ session }) {
-  //   const [loading, setLoading] = useState(true);
-  //   const [username, setUsername] = useState(null);
-  //   // const [website, setWebsite] = useState(null);
-  //   const [avatar_url, setAvatarUrl] = useState(null);
-
-  //   useEffect(() => {
-  //     getProfile();
-  //   }, [session]);
-
-  //   async function getProfile() {
-  //     try {
-  //       setLoading(true);
-  //       const user = supabase.auth.user();
-
-  //       let { data, error, status } = await supabase
-  //         .from("profiles")
-  //         .select(`username, avatar_url`)
-  //         .eq("id", user.id)
-  //         .single();
-
-  //       if (error && status !== 406) {
-  //         throw error;
-  //       }
-
-  //       if (data) {
-  //         setUsername(data.username);
-  //         // setWebsite(data.website);
-  //         setAvatarUrl(data.avatar_url);
-  //       }
-  //     } catch (error) {
-  //       alert(error.message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-
-  //   async function updateProfile({ username, avatar_url }) {
-  //     try {
-  //       setLoading(true);
-  //       const user = supabase.auth.user();
-
-  //       const updates = {
-  //         id: user.id,
-  //         username,
-  //         avatar_url,
-  //         updated_at: new Date(),
-  //       };
-
-  //       let { error } = await supabase.from("profiles").upsert(updates, {
-  //         returning: "minimal", // Don't return the value after inserting
-  //       });
-
-  //       if (error) {
-  //         throw error;
-  //       }
-  //     } catch (error) {
-  //       alert(error.message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-
   return (
-    <div className="form-widget">
-      <p>
-        Go to <Link to="/me-tree">MeTree</Link>
-        <Link to="/whose-playing"> whose </Link>
-        <Link to="/child-profile">child </Link>
-        <Link to="/login">login </Link>
-        <Link to="/">adult </Link>
-      </p>
-    </div>
+    <>
+      <div className="form-widget">
+        <p>
+          Go to <Link to="/me-tree">MeTree</Link>
+          <Link to="/whose-playing"> whose </Link>
+          <Link to="/child-profile">child </Link>
+          <Link to="/login">login </Link>
+          <Link to="/">adult </Link>
+        </p>
+      </div>
+
+      <div className="flex">
+        <Toolkit>
+          <ToolkitButton>
+            <ToolkitText>Add photo</ToolkitText>
+          </ToolkitButton>
+          <ToolkitButton>
+            <BtnImage src={WhatColour} alt="" />
+            <ToolkitText>Change background</ToolkitText>
+          </ToolkitButton>
+          <ToolkitButton>
+            <BtnImage src={WhatGrows} alt="" />
+            <ToolkitText>What's growing</ToolkitText>
+          </ToolkitButton>
+          <ToolkitButton>
+            <BtnImage src={WhoAround} alt="" />
+            <ToolkitText>Who is around</ToolkitText>
+          </ToolkitButton>
+          <ToolkitButton>
+            <BtnImage src={WhereTree} alt="" />
+            <ToolkitText>Where is your tree</ToolkitText>
+          </ToolkitButton>
+          <ToolkitButton>
+            <ToolkitText>Print</ToolkitText>
+          </ToolkitButton>
+        </Toolkit>
+
+        <div className="flex column center text-center items-center">
+          <h2>Welcome back Nicky and Ben!</h2>
+          <p className="narrow">
+            Here’s your Me Tree from last time - it’s looking good! Would you
+            like to change anything?
+          </p>
+
+          <MeTreeImage src={MeTreeGarden} alt="" />
+          <MeTreeImage src={MeTreePlanet} alt="" />
+          <MeTreeImage src={MeTreeHeart} alt="" />
+          <MeTreeImage src={MeTreeCloud} alt="" />
+        </div>
+      </div>
+    </>
   );
 }
