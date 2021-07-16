ee78ac25f209c1c27b9667abac1b4b1a6a872561
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-16 10:50:20 +0100

    Move login svg to public folder to try and fix deployed version
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

--- src/Layout/Login.styled.js
+++ src/Layout/Login.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const LoginTree = styled.div`
-  background-image: url("./../../../assets/login.svg");
+  background-image: url("/login.svg");
   background-size: cover;
   background-position: center;
   height: 100vh;
