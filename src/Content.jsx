012eb08e2e06b04fe94d35c53d1105d50de4b0f9
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-26 11:26:26 +0100

    Add content page route
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import { supabase } from "./supabaseClient";
+import { Link } from "react-router-dom";
+
+export default function Content() {
+  return (
+    <div>
+      <p>Content here</p>
+    </div>
+  );
+}
