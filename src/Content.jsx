147d536329b143e209aa05188af314eb932e735e
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-08-02 12:35:04 +0100

    Add reducer and context to keep track of sub-section state
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,67 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+} from "react";
 // import { supabase } from "./supabaseClient";
 import { Link } from "react-router-dom";
 import NavMenu from "./components/NavMenu";
 import ContentNav from "./components/ContentNav";
 import ContentTopBar from "./components/ContentTopBar";
 
+const initialState = {
+  current_section: "your brain is amazing",
+  current_subsection: "play",
+  unlocked: ["great to meet you", "your brain is amazing"],
+};
+console.log("initial state content", initialState);
+
+export const ACTIONS = {
+  UPDATE_CURRENT_SECTION: "update_current_section",
+  ADD_TO_UNLOCKED: "add_to_unlocked",
+  UPDATE_CURRENT_SUB_SECTION: "update_current_sub_section",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.UPDATE_CURRENT_SECTION:
+      const current_section = action.new_section;
+      return { ...state, current_section };
+    case ACTIONS.ADD_TO_UNLOCKED:
+      const unlocked = [...state.unlocked, action.new_unlocked];
+      return { ...state, unlocked };
+    case ACTIONS.UPDATE_CURRENT_SUB_SECTION:
+      console.log("updating subsection");
+      const current_subsection = action.new_sub_section;
+      console.log(current_subsection);
+      return { ...state, current_subsection };
+    default:
+      return state;
+  }
+}
+
+export const ContentContext = createContext();
+
 export default function Content() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    console.log("content top bar state: ", state);
+  }, [state]);
+
   return (
     <>
-      <div className="content--top-nav">
-        <ContentTopBar />
-        <ContentNav />
-        <NavMenu />
-      </div>
-      <div>{/* <p>Content here</p> */}</div>
+      <ContentContext.Provider value={{ state, dispatch }}>
+        <div className="content--top-nav">
+          <ContentTopBar />
+          <ContentNav />
+          <NavMenu />
+        </div>
+        <div>{/* <p>Content here</p> */}</div>
+      </ContentContext.Provider>
     </>
   );
 }
--- src/components/ContentNav.jsx
+++ src/components/ContentNav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Menu from "react-burger-menu/lib/menus/slide";
 // import "./../Layout/NavMenu.css";
 import "./../Layout/ContentNav.css";
+import Collapsible from "react-collapsible";
 
 export default function ContentNav() {
   return (
@@ -9,13 +10,149 @@ export default function ContentNav() {
       // noOverlay
       burgerButtonClassName={"content-burger-btn"}
       customBurgerIcon={<img src="logo.svg" />}
+      itemListClassName={"content-item-list"}
+      itemClassName={"content-item"}
     >
-      <a id="home" className="menu-item" href="/">
-        Think Bank
-      </a>
-      <a id="adult-profile" className="menu-item" href="/adult-profile">
-        Wonder Tree
-      </a>
+      <Collapsible trigger="Great to meet you">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="Your brain is amazing">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="Your feelings matter">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You're not alone">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You're safe">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You're unique">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You're brave">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You belong here">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="The future is bright">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
+      <Collapsible trigger="You are a wonder">
+        <a id="home" className="menu-item" href="#">
+          Play Den
+        </a>
+        <a id="home" className="menu-item" href="#">
+          Think Bank
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Make Station
+        </a>
+        <a id="adult-profile" className="menu-item" href="#">
+          Wonder Tree
+        </a>
+      </Collapsible>
     </Menu>
   );
 }
