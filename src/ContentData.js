f487551b84406263e3cd3f0e9ed52373745a1eb2
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-08-09 18:14:11 +0100

    Add hover effect to content top bar and additional content to contentData
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

--- src/ContentData.js
+++ src/ContentData.js
@@ -82,52 +82,56 @@ export const ContentData = {
     think: {
       1: {
         img: "assets/brain-amazing/Your_brain_is_amazing_2.svg",
-        txt: "Hello! We're your brain creatures. It's great to meet you!",
+        txt: "Hello! We're your brain creatures. It's great to meet you! We have a safety bit. We have a feeling bit. And we have a thinking bit!",
       },
       2: {
         img: "assets/brain-amazing/Your_brain_is_amazing_4.svg",
-        txt: "The thinking bit is where the thinking buds live...",
+        txt: "The thinking bit is where the thinking buds live. We help you learn and come up with new ideas. We're really clever, and interesting. When we feel safe, and calm, we can learn: music, dance, coding and more! It's so exciting when us thinking buds start to blossom!",
       },
       3: {
+        img: "assets/brain-amazing/Your_brain_is_amazing_8.svg",
+        txt: "I wonder what wonderful ideas the thinking buds in your brain are thinking?",
+      },
+      4: {
         img: "assets/brain-amazing/Your_brain_is_amazing_13.svg",
         txt: "The feeling bit of your brain helps you respond to the world around you. We are like lots of little computers sending and receiving messages to the rest of your body.",
       },
-      4: {
+      5: {
         img: "assets/brain-amazing/Your_brain_is_amazing_14.svg",
         txt: "",
       },
-      5: {
+      6: {
         img: "assets/brain-amazing/Your_brain_is_amazing_15.svg",
         txt: "",
       },
-      6: {
+      7: {
         img: "assets/brain-amazing/Your_brain_is_amazing_19.svg",
         txt: "I’m Safety Sensor, and I live in the safety bit of your brain. I look out for signs of safety and danger.  I won’t let you think or feel if there is danger around. I have an alarm that goes off when they think you’re in danger.",
       },
-      7: {
+      8: {
         img: "assets/brain-amazing/Your_brain_is_amazing_5.svg",
-        txt: "ADDD",
+        txt: "",
       },
     },
     make: {
       1: {
-        img: "assets/simple_make.svg",
-        txt: "Welcome back to the Making Station! Today, you are invited to make a calm bag to keep your amazing brain happy!!",
+        img: "assets/make_4_sensory_bag.svg",
+        txt: "Today, you are invited to make a calm bag to keep your amazing brain happy!!",
       },
       2: {
-        img: "assets/SENSORY_BAG.svg",
+        img: "assets/sensory_bag_make_1.svg",
         txt: "In your toolkit, you should find a golden bag, and some cut out shapes. Click the arrow when you’ve found them…",
       },
       3: {
-        img: "assets/simple_make.svg",
+        img: "assets/star_make_2.svg",
         txt: "On the star cut-out, you’re invited to write or draw your star qualities.  Some people find it really tough to see their own good bits - your grown up is there to help you see how amazing you are.",
       },
       4: {
-        img: "assets/simple_make.svg",
+        img: "assets/make_cloud_3.svg",
         txt: "On the cloud, you’re invited to write or draw all the things that help you feel calm.  You might want to look through magazines and cut out pictures and words.",
       },
       5: {
-        img: "assets/simple_make.svg",
+        img: "assets/SENSORY_BAG.svg",
         txt: "In the bag, you are invited to put your star and cloud, as well as any fabrics, fidget toys, and other things that help you feel calm.  ",
       },
     },
