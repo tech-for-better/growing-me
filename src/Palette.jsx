37bed5157c0ee71881200cd13d9bba041b804bef
Safia <safiaali@hotmail.com>
2021-08-11 02:38:02 +0100

    Implemented Oli's suggestions to Palette, so that when you're mapping over the options you can pass the update type in to your click handler: Store which update type goes with which image in paletteOptions and a static image URL so you can map each image to a specific path in the switch statement

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -44,62 +44,138 @@ export default function Palette({ type }) {
   };
 
   let option = type;
-  let paletteOptions = {
+  // let paletteOptions = {
+  //   WhatColour: [
+  //     imagesToSupabaseMapping[mountainBlob],
+  //     mountainBlob,
+  //     spikeyBlob,
+  //     minecraftBlob,
+  //     jellyBlob,
+  //     heartBlob,
+  //     cloudyBlob,
+  //     ovalBlob,
+  //   ],
+  //   WhatGrows: [apple, banana, batwings, cherries, chocolate, pizza],
+  //   WhoAround: [
+  //     cuteVisitor,
+  //     pricklyVisitor,
+  //     fluffyVisitor,
+  //     creepyCrawlyVisitor,
+  //     worm,
+  //   ],
+  //   WhereTree: [MeTreeGarden, MeTreeCloud, MeTreeHeart, MeTreePlanet],
+  // };
+
+  const paletteOptions = {
     WhatColour: [
-      imagesToSupabaseMapping[mountainBlob],
-      mountainBlob,
-      spikeyBlob,
-      minecraftBlob,
-      jellyBlob,
-      heartBlob,
-      cloudyBlob,
-      ovalBlob,
+      { src: mountainBlob, update: "update_background" },
+      { src: spikeyBlob, update: "update_background" },
+      { src: minecraftBlob, update: "update_background" },
+      { src: jellyBlob, update: "update_background" },
+      { src: heartBlob, update: "update_background" },
+      { src: cloudyBlob, update: "update_background" },
+      { src: ovalBlob, update: "update_background" },
     ],
-    WhatGrows: [apple, banana, batwings, cherries, chocolate, pizza],
+    WhatGrows: [
+      { src: apple, update: "update_growing" },
+      { src: banana, update: "update_growing" },
+      { src: batwings, update: "update_growing" },
+      { src: cherries, update: "update_growing" },
+      { src: chocolate, update: "update_growing" },
+      { src: pizza, update: "update_growing" },
+    ],
     WhoAround: [
-      cuteVisitor,
-      pricklyVisitor,
-      fluffyVisitor,
-      creepyCrawlyVisitor,
-      worm,
+      { src: cuteVisitor, update: "update_whoAround" },
+      { src: pricklyVisitor, update: "update_whoAround" },
+      { src: fluffyVisitor, update: "update_whoAround" },
+      { src: creepyCrawlyVisitor, update: "update_whoAround" },
+      { src: worm, update: "update_whoAround" },
     ],
-    WhereTree: [MeTreeGarden, MeTreeCloud, MeTreeHeart, MeTreePlanet],
+    WhereTree: [
+      { src: MeTreeGarden, update: "update_treeLocation" },
+      { src: MeTreeCloud, update: "update_treeLocation" },
+      { src: MeTreeHeart, update: "update_treeLocation" },
+      { src: MeTreePlanet, update: "update_treeLocation" },
+    ],
   };
 
-  const imgToDispatchTypeMapping = {
-    "where_-_cloud.svg": "update_treeLocation",
-    "where_-_garden.svg": "update_treeLocation",
-    "where_-_on_a_big_love_heart.svg": "update_treeLocation",
-    "where_-_another_planet.svg": "update_treeLocation",
-    "cute_visitors.svg": "update_whoAround",
-    "prickly_visitors.svg": "update_whoAround",
-    "fluffy_visitors.svg": "update_whoAround",
-    "creepy_crawly_visitors.svg": "update_whoAround",
-    "home_for_worms.svg": "update_whoAround",
-    "growing_apples.svg": "update_growing",
-    "growing_bananas.svg": "update_growing",
-    "growing_batwings.svg": "update_growing",
-    "growing_chocolate.svg": "update_growing",
-    "growing_cherries.svg": "update_growing",
-    "growing_pizza.svg": "update_growing",
-    "mountain_blob.svg": "update_background",
-    "spikey_blob.svg": "update_background",
-    "minecraft_blob.svg": "update_background",
-    "jelly_blob.svg": "update_background",
-    "heart_blob.svg": "update_background",
-    "cloudy_blob.svg": "update_background",
-    "oval_blob.svg": "update_background",
-  };
+  console.log("palletteOptions[option]",paletteOptions[option])
+  // const imgToDispatchTypeMapping = {
+  //   "where_-_cloud.svg": "update_treeLocation",
+  //   "where_-_garden.svg": "update_treeLocation",
+  //   "where_-_on_a_big_love_heart.svg": "update_treeLocation",
+  //   "where_-_another_planet.svg": "update_treeLocation",
+  //   "cute_visitors.svg": "update_whoAround",
+  //   "prickly_visitors.svg": "update_whoAround",
+  //   "fluffy_visitors.svg": "update_whoAround",
+  //   "creepy_crawly_visitors.svg": "update_whoAround",
+  //   "home_for_worms.svg": "update_whoAround",
+  //   "growing_apples.svg": "update_growing",
+  //   "growing_bananas.svg": "update_growing",
+  //   "growing_batwings.svg": "update_growing",
+  //   "growing_chocolate.svg": "update_growing",
+  //   "growing_cherries.svg": "update_growing",
+  //   "growing_pizza.svg": "update_growing",
+  //   "mountain_blob.svg": "update_background",
+  //   "spikey_blob.svg": "update_background",
+  //   "minecraft_blob.svg": "update_background",
+  //   "jelly_blob.svg": "update_background",
+  //   "heart_blob.svg": "update_background",
+  //   "cloudy_blob.svg": "update_background",
+  //   "oval_blob.svg": "update_background",
+  // };
+  // when runs it'll end up being something like
+  // switch ("/assets/banana.123abc.svg") {
+  //   case "/assets/apple.123abc.svg":
+  //   case "/assets/banana.123abc.svg":
+  //   // ...
+  // }
+
+  // async function handleClick(event, image) {
+  //   console.log("clicked a palette button", event, image);
+  //   let imageFileName = getShortImagePath(image);
+  //   let dispatchType = imgToDispatchTypeMapping[imageFileName];
+
+  //   switch (dispatchType) {
+  //     case "update_treeLocation":
+  //       setState({
+  //         tree: {
+  //           tree_location: event.target.src,
+  //         },
+  //       });
+  //       break;
+  //     case "update_background":
+  //       setState({
+  //         tree: {
+  //           background: event.target.src,
+  //         },
+  //       });
+  //       break;
+  //     case "update_growing":
+  //       setState({
+  //         tree: {
+  //           growing: event.target.src,
+  //         },
+  //       });
+  //       break;
+  //     case "update_whoAround":
+  //       setState({
+  //         tree: {
+  //           who_around: event.target.src,
+  //         },
+  //       });
+  //       break;
+  //   }
+  // }
 
   async function handleClick(event, image) {
     console.log("clicked a palette button", event, image);
-    let imageFileName = getShortImagePath(image);
-    let dispatchType = imgToDispatchTypeMapping[imageFileName];
-    switch (dispatchType) {
+
+    switch (image) {
       case "update_treeLocation":
         setState({
           tree: {
-            tree_location: event.target.src,
+            tree_location: image,
           },
         });
         break;
@@ -129,7 +205,7 @@ export default function Palette({ type }) {
 
   return (
     <>
-      <PaletteContainer>
+      {/* <PaletteContainer>
         {paletteOptions[option].map((image) => {
           return (
             <>
@@ -139,6 +215,21 @@ export default function Palette({ type }) {
                 onClick={(event) => handleClick(event, image)}
               >
                 <PaletteImg key={image} src={image} alt={image} />
+              </PaletteBtn>
+            </>
+          );
+        })}
+      </PaletteContainer> */}
+      <PaletteContainer>
+        {paletteOptions[option].map(({ src, update }) => {
+          return (
+            <>
+              <PaletteBtn
+                key={src}
+                image={src}
+                onClick={(event) => handleClick(event, update)}
+              >
+                <PaletteImg key={src} src={src} alt={src} />
               </PaletteBtn>
             </>
           );
