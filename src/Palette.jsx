c8b9407dc127e53a479ac907e34e2e4435d66e93
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-08-11 10:59:47 +0100

    Remove full path for assets because unnecessary
    Co-authored-by: Safia Ali <safiaali@hotmail.com>

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -5,28 +5,28 @@ import {
   PaletteImg,
   PaletteBtn,
 } from "./Layout/Palette.styled";
-import MeTreeGarden from "./../public/assets/where_-_garden.svg";
-import MeTreeCloud from "./../public/assets/where_-_cloud.svg";
-import MeTreeHeart from "./../public/assets/where_-_on_a_big_love_heart.svg";
-import MeTreePlanet from "./../public/assets/where_-_another_planet.svg";
-import cuteVisitor from "./../public/assets/cute_visitors.svg";
-import pricklyVisitor from "../public/assets/prickly_visitors.svg";
-import fluffyVisitor from "./../public/assets/fluffy_visitors.svg";
-import creepyCrawlyVisitor from "./../public/assets/creepy_crawly_visitors.svg";
-import worm from "./../public/assets/home_for_worms.svg";
-import apple from "./../public/assets/growing_apples.svg";
-import banana from "./../public/assets/growing_bananas.svg";
-import batwings from "./../public/assets/growing_batwings.svg";
-import cherries from "./../public/assets/growing_cherries.svg";
-import chocolate from "./../public/assets/growing_chocolate.svg";
-import pizza from "./../public/assets/growing_pizza.svg";
-import mountainBlob from "./../public/assets/mountain_blob.svg";
-import spikeyBlob from "./../public/assets/spikey_blob.svg";
-import minecraftBlob from "./../public/assets/minecraft_blob.svg";
-import jellyBlob from "./../public/assets/jelly_blob.svg";
-import heartBlob from "./../public/assets/heart_blob.svg";
-import cloudyBlob from "./../public/assets/cloudy_blob.svg";
-import ovalBlob from "./../public/assets/oval_blob.svg";
+import MeTreeGarden from "/assets/where_-_garden.svg";
+import MeTreeCloud from "/assets/where_-_cloud.svg";
+import MeTreeHeart from "/assets/where_-_on_a_big_love_heart.svg";
+import MeTreePlanet from "/assets/where_-_another_planet.svg";
+import cuteVisitor from "/assets/cute_visitors.svg";
+import pricklyVisitor from "/assets/prickly_visitors.svg";
+import fluffyVisitor from "/assets/fluffy_visitors.svg";
+import creepyCrawlyVisitor from "/assets/creepy_crawly_visitors.svg";
+import worm from "/assets/home_for_worms.svg";
+import apple from "/assets/growing_apples.svg";
+import banana from "/assets/growing_bananas.svg";
+import batwings from "/assets/growing_batwings.svg";
+import cherries from "/assets/growing_cherries.svg";
+import chocolate from "/assets/growing_chocolate.svg";
+import pizza from "/assets/growing_pizza.svg";
+import mountainBlob from "/assets/mountain_blob.svg";
+import spikeyBlob from "/assets/spikey_blob.svg";
+import minecraftBlob from "/assets/minecraft_blob.svg";
+import jellyBlob from "/assets/jelly_blob.svg";
+import heartBlob from "/assets/heart_blob.svg";
+import cloudyBlob from "/assets/cloudy_blob.svg";
+import ovalBlob from "/assets/oval_blob.svg";
 
 import { MeTreeContext } from "./App";
 
